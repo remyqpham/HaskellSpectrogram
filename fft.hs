a2eca857dbaac4506834dbf49d10d275b7a80d32
anthonyassi <aassi@ucsc.edu>
2016-02-15 16:11:45 -0800

    getFund

--- fft.hs
+++ fft.hs
@@ -3,14 +3,19 @@ import Data.WAVE
 import Data.Int (Int32)
 import Data.List.Split (splitOn)
 import Data.Complex
+import Data.List
 
 main = do
     arg <- getArgs
     w <- getWAVEFile (head arg)
-    print (getMag (fft (head (plexifyList (waveToChan (getSamples w))))))
-
+    print (getFund (getMag (fft (head (plexifyList (waveToChan (getSamples w)))))))
+ --   print (headerToString (getHeader w))
 getSamples :: WAVE -> WAVESamples
 getSamples (WAVE _ w) = w
+
+largest :: [Double] -> Double
+largest [] = 0
+largest (head : tail) = max head (largest tail)
 
 --returns [[L0,L1...Ln],[R0,R1...Rn]]
 waveToChan :: WAVESamples -> [[Double]]
@@ -37,13 +42,19 @@ toComplex x = x :+ 0
 plexifyList :: [[Double]] -> [[Complex Double]]
 plexifyList xs = (toComplex <$>) <$> xs
 
+getFund :: [Double] -> Maybe Double
+getFund ds = (*t) <$> i
+    where n = length ds
+          i = fromIntegral <$> elemIndex (maximum ds) ds
+          t = 44100 / (fromIntegral n)
+
 getHeader :: WAVE -> WAVEHeader
 getHeader (WAVE h _) = h
 
 headerToString :: WAVEHeader -> String
 headerToString (WAVEHeader c fps bps f) = "Number of channels: "++ show c
     ++ "\nSample rate: " ++ show fps ++ "\nBit depth: "
-    ++ show bps ++ "\n"
+    ++ show bps ++ "\nNumber of frames: " ++ show f ++ "\n"
 
 -- Cooley-Tukey
 fft :: [Complex Double] -> [Complex Double]

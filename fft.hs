e7977efdfbae640d31d3baecb04ccfb2413e0ba2
anthonyassi <aassi@ucsc.edu>
2016-02-16 20:55:59 -0800

    getBandMag

--- fft.hs
+++ fft.hs
@@ -4,12 +4,13 @@ import Data.Int (Int32)
 import Data.List.Split (splitOn)
 import Data.Complex
 import Data.List
+import Data.List.Split
 
 main = do
     arg <- getArgs
     w <- getWAVEFile (head arg)
-    putStr (fftToStr (getMag (fft (head (plexifyList (waveToChan (getSamples w)))))))
- --   print (headerToString (getHeader w))
+    print (getBandMag 200 (getMag (fft (head (plexifyList (waveToChan (getSamples w)))))))
+
 getSamples :: WAVE -> WAVESamples
 getSamples (WAVE _ w) = w
 
@@ -43,6 +44,17 @@ freqToMag f fft = fft !! (round (f * n / fs))
     where n =  fromIntegral (length fft)
           fs = 44100 
 
+--returns list b+1 magnitudes for b+1 freq bin from 0-20,000 Hz
+getBandMag :: Int -> [Double] -> [Double]
+getBandMag b f = map sum fftSplit
+    where n = length f
+          fs = 44100
+          minI = quot (20 * n) fs  -- min index when f=20Hz
+          maxI = quot (20000 * n) fs  -- max index when f=20kHz
+          fft = fst (splitAt maxI f)  --ret freqs < 20,000 Hz 
+          fftSplit = chunksOf (quot (length fft) b) fft  --splits fft to b+1 bins 
+
+--returns fundemental freq of fft
 getFund :: [Double] -> Maybe Double
 getFund ds = (*t) <$> i
     where n = length ds

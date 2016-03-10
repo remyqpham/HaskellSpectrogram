9fa887652eba09e4fb4309bb8b86d634d17160be
anthonyassi <aassi@ucsc.edu>
2016-03-10 15:33:49 -0800

    comments

--- fft.hs
+++ fft.hs
@@ -11,23 +11,25 @@ import System.IO
 
 main = do arg <- getArgs
           w <- getWAVEFile (head arg)
-          writeFile "noise.txt" (toSpectro w 0 100 200)
---          putStr (toSpectro w 0 100 200) 
+          writeFile "bird.txt" (toSpectro w 0 100 200) 
 
+-- Main function that takes in the args below and writes output data to file 
 -- .wav -> channel -> timebins -> freqbins -> string
 toSpectro :: WAVE -> Int -> Int -> Int -> String
-toSpectro w i t f = (toPrint(map (\l -> getBandMag f l) (getDB (fftBins ((plexifyList (waveToChan (getSamples w))) !! i) (getBinSize (getHeader w) t)))))
+toSpectro w i t f = (toPrint(map (\l -> getBandDB f l) (getDB (fftBins ((plexifyList (waveToChan (getSamples w))) !! i) (getBinSize (getHeader w) t)))))
 
---puts list of doubles as strings separated by commas
+--turns a list of doubles into a line of strings separated by commas
 toPrintLine :: [Double] -> String
 toPrintLine ds = foldr (\d a -> (show d) ++ "," ++ a) [] ds
 
+--turns each list of doubles into a line of strings separted by commas 
 toPrint :: [[Double]] -> String
 toPrint ls = foldr (\l a -> (toPrintLine l) ++ "\n" ++ a) [] ls
 
 getSamples :: WAVE -> WAVESamples
 getSamples (WAVE _ w) = w
 
+-- breaks an audio file(ds) into (n) time bins
 fftBins :: [Complex Double] ->Int -> [[Complex Double]]
 fftBins ds n = fst (splitAt 100 (map fft (chunksOf n ds)))
 
@@ -55,7 +57,6 @@ frameToDoubles (s:ss) = (sampleToDouble s):(frameToDoubles ss)
 
 --DB =10*log10(sqrt(re^2 + im^2))
 getDB :: [[Complex Double]] -> [[Double]]
---getDB ls = map (\ds -> (map (\d -> 10*(logBase 10 (sqrt((((realPart d)^2)+((imagPart d)^2))/(fromIntegral (length ds)))))) ds)) ls
 getDB ls = map (\ds -> (map (\d -> 10*(logBase 10 (sqrt(((realPart d)^2)+((imagPart d)^2))))) ds)) ls
 
 toComplex :: Double -> Complex Double
@@ -64,14 +65,9 @@ toComplex x = x :+ 0
 plexifyList :: [[Double]] -> [[Complex Double]]
 plexifyList xs = (toComplex <$>) <$> xs
 
-freqToMag :: Double -> [Double] -> Double
-freqToMag f fft = fft !! (round (f * n / fs))
-    where n =  fromIntegral (length fft)
-          fs = 44100 
-
---returns list b+1 magnitudes for b+1 freq bin from 0-20,000 Hz
-getBandMag :: Int -> [Double] -> [Double]
-getBandMag b f = map sum fftSplit
+--returns list b DB's for b freq bin from 0-20,000 Hz
+getBandDB :: Int -> [Double] -> [Double]
+getBandDB b f = map sum fftSplit
     where n = length f
           fs = 44100
           minI = quot (20 * n) fs  -- min index when f=20Hz
@@ -79,16 +75,10 @@ getBandMag b f = map sum fftSplit
           fft = fst (splitAt maxI f)  --ret freqs < 20,000 Hz 
           fftSplit = chunksOf (quot (length fft) b) fft  --splits fft to b+1 bins 
 
---returns fundemental freq of fft
-getFund :: [Double] -> Maybe Double
-getFund ds = (*t) <$> i
-    where n = length ds
-          i = fromIntegral <$> elemIndex (maximum ds) ds
-          t = 44100 / (fromIntegral n)
-
 getHeader :: WAVE -> WAVEHeader
 getHeader (WAVE h _) = h
 
+--prints .wav file metadata 
 headerToString :: WAVEHeader -> String
 headerToString (WAVEHeader c fps bps f) = "Number of channels: "++ show c
     ++ "\nSample rate: " ++ show fps ++ "\nBit depth: "
